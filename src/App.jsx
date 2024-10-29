1c6927a5ff776744175a7956077c000466be7843
Murshyne <murshyne@gmail.com>
2024-10-28 23:36:10 -0400

    Committed at: Mon, Oct 28, 2024 11:36:09 PM

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { learners } from './components/utility/data.mjs';
+import Learner from './components/Learner/Learner';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [learnerData] = useState({learners});
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+ {learnerData.learners.map((learner, index) => (
+        <Learner key={index} learner={learner} />
+      ))}
     </>
   )
 }
 
-export default App
+export default App;
--- src/components/utility/data.mjs
+++ src/components/utility/data.mjs
@@ -1,4 +1,4 @@
-[
+export const learners = [
   {
     name: "Cait Yomorta",
     bio: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus placeat nostrum explicabo? Voluptatibus expedita saepe officia optio, commodi totam ratione laudantium ipsum porro molestias, quasi nulla minus vitae laboriosam corrupti Delectus inventore explicabo est odit incidunt rem a recusandae eum pariatur. Aperiam doloremque blanditiis harum voluptate animi fugit beatae asperiores quo, dignissimos sed illum veniam eum accusantium nulla quod voluptatum",
@@ -58,4 +58,3 @@
     ],
   },
 ];
-export default data;
